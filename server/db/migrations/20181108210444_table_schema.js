8173526434effc0471fbecff5a9d72ed93ba05f5
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-11-08 22:55:23 -1000

    foreign keys handled

--- server/db/migrations/20181108210444_table_schema.js
+++ server/db/migrations/20181108210444_table_schema.js
@@ -1,28 +1,6 @@
 
 exports.up = function (knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('Users', (table) => {
-      table.increments('id').primary();
-      table.string('name').notNullable();
-      table.string('last').notNullable();
-      table.string('email').unique().notNullable();
-      table.string('password').notNullable();
-      table.integer('phone').unique().notNullable();
-      table.boolean('diet').notNullable();
-      table.timestamp('created_at').defaultTo(knex.fn.now())
-      table.integer('location_id').references('id').inTable('Local').notNullable()
-    }),
-    knex.schema.createTable('Local', (table) => {
-      table.increments('id').primary();
-      table.string('address').notNullable();
-      table.string('city').notNullable();
-      table.string('state').notNullable();
-      table.string('zip').notNullable();
-      table.integer('local_lat').notNullable();
-      table.integer('local_long').notNullable();
-      table.integer('local_id')
-        .references('Food.id');
-    }),
     knex.schema.createTable('Food', (table) => {
       table.increments('id').primary();
       table.string('category').notNullable();
@@ -33,15 +11,45 @@ exports.up = function (knex, Promise) {
       table.integer('food_lat').notNullable();
       table.integer('food_long').notNullable();
       table.timestamp('created_at').defaultTo(knex.fn.now())
-      table.integer('user_id').references('id').inTable('Users').notNullable()
+      table.integer('user_id')
+        .notNullable()
+        .references('id')
+        .inTable('Users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    }),
+    knex.schema.createTable('Users', (table) => {
+      table.increments('id').primary();
+      table.string('name').notNullable();
+      table.string('last').notNullable();
+      table.string('email').unique().notNullable();
+      table.string('password').notNullable();
+      table.integer('phone').unique().notNullable();
+      table.boolean('diet').notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now())
+      table.integer('location_id')
+        .notNullable()
+        .references('id')
+        .inTable('Local')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    }),
+    knex.schema.createTable('Local', (table) => {
+      table.increments('id').primary()
+      table.string('address').notNullable()
+      table.string('city').notNullable()
+      table.string('state').notNullable()
+      table.string('zip').notNullable()
+      table.integer('local_lat').notNullable()
+      table.integer('local_long').notNullable()
     })
   ]);
 };
 
 exports.down = function (knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('Users').onDelete('cascade'),
-    knex.schema.dropTable('Location').onDelete('cascade'),
-    knex.schema.dropTable('Food').onDelete('cascade')
+    knex.schema.dropTable('Food'),
+    knex.schema.dropTable('Users'),
+    knex.schema.dropTable('Local')
   ])
 };

10366c55e8d6d0aff8a02ee12ab35202b5b2b80a
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-11-09 18:37:56 -1000

    📖  DOC: front-end-architecture finished

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../containers/App/App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+const Header = () => {
+    return (
+        <div >
+            <Router>
+                <div>
+                    <div className='header-bar'>
+                        <Link className='App-title' to='/'>
+                            <p>Chilema</p>
+                        </Link>
+                        <Link className='App-title' to='/login'>
+                            <button>Log In</button>
+                        </Link>
+                        <Link className='App-title' to='/newuser'>
+
+                            <button>Sign Up</button>
+                        </Link>
+                    </div>
+                    <div className='body-container'>
+                        <Route path='/' component={App} />
+                        <Route path='/login' component={LogIn} />
+                        <Route path='/newuser' component={NewUser} />
+                    </div>
+                </div>
+            </Router>
+        </div>
+
+    );
+};
+
+
+//temp components to test on router
+function App(props) {
+    return <div>this is home page</div>
+}
+function LogIn(props) {
+    return <div>login page....</div>
+}
+function NewUser(props) {
+    return <div>Sign up plz</div>
+}
+
+export default Header;

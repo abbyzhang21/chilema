280a50650e697adbd78cab149fe63e65980db650
Sarah <yamashige.sarah@gmail.com>
2018-11-10 19:02:13 -1000

    Set up a Home Page - need to add router

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,3 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { SearchContainer } from '../../components/Search';
+import './Home.css';
 
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            foodItem: []
+        }
+    }
+
+    componentDidMount() {
+        axios 
+            .get('/food')
+            .then(foods => {
+                console.log('foodItem', foods.data)
+            this.setState({foodItem:foods.data})    
+            })
+            .catch(err => {
+                console.log('err', err)
+        })
+    }
+
+    render() {
+        return (
+            <div className="Home-Container">
+                <SearchContainer foodItem={this.state.foodItem}/>
+            </div>
+        )
+    }
+}
+
+export default Home;

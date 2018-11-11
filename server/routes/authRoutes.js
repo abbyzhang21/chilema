12ba4244a40f9a5487fd2261280fc50cb416c3c2
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-11-11 00:09:40 -1000

    trying different bcrypt approach

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,6 +4,64 @@ const authRouter = express.Router();
 // BOOKSHELF DATA MODELS
 const Users = require('../db/models/Users.js')
 
+// PASPORT & BCRYPT LIBRARIES
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcrypt')
+
+// upon successful login, get user from database, save
+// save user into Redis session store
+passport.serializeUser((user, done) => {
+  console.log('serializing user: ', user)
+  done(null, user.id)
+})
+
+// upon successful authorization request, take information from the session
+// for example userId, to retrieve the user record from the db, and put it into req.user
+passport.deserializeUser((user, done) => {
+  console.log('deserializing users: ', user)
+  // console.log(req.user)
+  done(null, {
+    id: user.attributes.id,
+    email: user.attributes.email
+  })
+})
+
+// passport config
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  console.log('LS EMAIL: ', email)
+  console.log('LS PASSWORD: ', password)
+  Users
+    .where({ email })
+    .fetch()
+    .then((user) => {
+      console.log('local-storage user: ', user)
+      // return user
+      console.log('passport localStrategy config password: ', password)
+      console.log('passport localStrategy config user.attributes.password: ', user.attributes.password)
+      if (password === user.attributes.password) {
+        console.log(true)
+      }
+      bcrypt.compare(password, user.attributes.password)
+        .then((result) => {
+          console.log('passport localStrategy config result: ', result)
+          if (result === true) {
+            console.log('user has correct credentials')
+            done(null, user.attributes)
+          } else {
+            console.log('user has incorrect credentials')
+            done(null, false)
+          }
+        })
+        .catch(err => {
+          done(err)
+        })
+    })
+    .catch((err) => {
+      done(err)
+    })
+}))
+
 // ROUTES
 authRouter.get('/register', (req, res) => {
   res.json('get register page')
@@ -23,30 +81,20 @@ authRouter.post('/register', (req, res) => {
     })
 })
 
-// login user
-authRouter.post('/login', (req, res) => {
-  const { email, password } = req.body
-  Users
-    .where({ email })
-    .fetch()
-    .then(user => {
-      if (password === user.attributes.password) {
-        req.session.isAuthenticated = true
-        res.json("USER AUTHENTICATED")
-      }
-      else {
-        res.json("INCORRECT PASSWORD")
-      }
-    })
-    .catch(err => {
-      console.log('err', err)
-      res.json(err)
-    })
-})
+authRouter.post('/login', passport.authenticate('local', { sucessRedirect: '/welcome', failureRedirect: '/' }), (req, res) => {
+  console.log('USER AUTHENTICATED')
+  // req.session.isAuthenticated = true;
+  res.send('USER AUTHENTICATED')
+});
+
+// authRouter.post('/login/google', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+//   console.log('GOOGLE AUTHENTICATION')
+//   res.json('GOOGLE AUTHENTICATION')
+// })
 
 // logout user (implement res.redirect to /login page)
 authRouter.get('/logout', (req, res) => {
-  req.session.destroy()
+  req.logout()
   res.json('USER LOGGED OUT')
 })
 
--- server/server.js
+++ server/server.js
@@ -2,26 +2,37 @@ const express = require('express')
 const app = express()
 const PORT = 5000;
 
+// BOOKSHELF DATA MODELS
+const Users = require('./db/models/Users.js')
+const Local = require('./db/models/Local.js')
+const Food = require('./db/models/Food.js')
+
 // MIDDLEWARE
 const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
 const bodyParser = require('body-parser')
+const passport = require('passport')
+const bcrypt = require('bcrypt')
+
+// app.use(express.static('public'));
 
 // bodyParser config
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-//redis session store
+// redis session store
 app.use(session({
   store: new RedisStore({ url: 'redis://redis-session-store:6379', logErrors: true }),
   secret: 'appetito',
+  resave: false,
   saveUninitialized: true
 }))
 
-// BOOKSHELF DATA MODELS
-const Users = require('./db/models/Users.js')
-const Local = require('./db/models/Local.js')
-const Food = require('./db/models/Food.js')
+// passport config
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+// app.use(flash()); // use connect-flash for flash messages stored in session
+
 
 // ROUTES //
 const authRoutes = require('./routes/authRoutes.js')
@@ -29,7 +40,12 @@ app.use('/auth', authRoutes)
 
 // login-homepage
 app.get('/', (req, res) => {
-  res.json('hello world!')
+  res.json('HOMEPAGE REDIRECT')
+})
+
+// welcome-page (placeholder)
+app.get('/welcome', (req, res) => {
+  res.json('WELCOME PAGE REDIRECT')
 })
 
 // get all food items in database

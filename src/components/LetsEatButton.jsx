0e856b213a47ce86034ee0039ad3bec9e2a1cea5
Sarah <yamashige.sarah@gmail.com>
2018-11-13 22:35:40 -1000

    setting up a button component with general button css

---
+++ src/components/LetsEatButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../stylesheets/_buttons.css';
+
+export const LetsEatButtonComponent = () => {
+
+    return (
+        <div className="lets-eat-button-containter">
+            <button>LET'S EAT!</button>
+        </div>
+    )
+}

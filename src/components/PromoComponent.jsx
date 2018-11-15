e43d4a24a860119c27d1641b5092d443075a6f9d
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-11-15 12:25:01 -1000

    ✅  TEST: start to work on promo component

---
+++ src/components/PromoComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../stylesheets/_promo.css';
+
+export const Promo = (props) => {
+    console.log('promo item', props.foodItem);
+    return (
+        <div className='promo-container'>
+            promo area....
+        </div>
+    )
+}

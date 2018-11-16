4c74b02383f501f9b6056bc36fc4bb4648e18afc
Sarah <yamashige.sarah@gmail.com>
2018-11-15 18:38:38 -1000

    change a little styling on the payment page and button css file

--- src/containers/Payment/Payment.jsx
+++ src/containers/Payment/Payment.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { CardElement, injectStripe } from 'react-stripe-elements';
 import '../../stylesheets/_stripeElements.css';
 import { Elements, StripeProvider } from 'react-stripe-elements';
+import GlobalHeader from '../../components/GlobalHeaderComponent';
+import { DoneButtonComponent } from '../../components/ButtonComponents';
 
 class PaymentForm extends Component {
   constructor(props) {
@@ -18,20 +20,27 @@ class PaymentForm extends Component {
 
   render() {
     return (
-      <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
-        <div className="example">
-          <Elements>
-            <div className="checkoutWrapper">
-              <h1>Payment Form</h1>
-              <p>Would you like to complete the purchase?</p>
-              <CardElement />
-              <div id="strBtnWrapper">
-                <button class="button" onClick={this.submit}>Send</button>
+      <div className="payment-container">
+        <GlobalHeader />
+        <div className="stripe-container">
+          <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+          <div className="example">
+            <Elements>
+              <div className="checkoutWrapper">
+                  <h1>PAYMENT INFORMATION</h1>
+                <br/>  
+                <p>Would you like to complete the purchase?</p>
+                <CardElement />
+                {/* <div id="strBtnWrapper">
+                  <button class="button" onClick={this.submit}>Send</button>
+                </div> */}
+                <DoneButtonComponent/>  
               </div>
-            </div>
-          </Elements>
-        </div>
-      </StripeProvider>
+            </Elements>
+          </div>
+        </StripeProvider>
+        </div>  
+      </div>
     )
   }
 }

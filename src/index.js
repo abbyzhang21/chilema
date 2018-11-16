b541e621ab04f02b6938a456cb8db3ab235500b8
Sarah <yamashige.sarah@gmail.com>
2018-11-15 21:59:42 -1000

    linking things

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import App from './containers/App';
+import Reducer from './reducers/reducers.js';
+
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(Reducer);
 
 
 ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));

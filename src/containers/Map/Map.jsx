8a29de5b116b6fea7080050a7dea1946cdc172d1
Nicholas Gambino <nicholasgambino@Nicholass-MBP.home>
2018-11-15 16:19:20 -1000

    dynamic leaflet markers

--- src/containers/Map/Map.jsx
+++ src/containers/Map/Map.jsx
@@ -2,33 +2,64 @@ import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 import './Map.css'
 
+
+// axios call to database
+import axios from 'axios';
+
+
 class myMap extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      lat: 21.3068,
-      lng: -157.8607,
-      zoom: 13,
-    }
+    this.state =
+      []
+    // {
+    //   lat: "21.3068",
+    //   lng: "-157.8607",
+    //   zoom: 13,
+    // }
   }
 
-  componentDidMount() { }
+  componentDidMount() {
+    axios
+      .get('/local')
+      .then(local => {
+        // console.log(food.data)
+        local.data.map((item) => {
+          let location = [
+            item.local_lat,
+            item.local_long
+          ]
+          this.state.push(location)
+          // console.log('THIS.STATE: ', this.state)
+          this.state.forEach((element) => {
+            console.log("MAP ELEMENT: ", element)
+          })
+        })
+      })
+      .catch(err => {
+        console.log('err', err)
+      })
+  }
 
 
   render() {
-    const position = [this.state.lat, this.state.lng]
+    const position = ["21.3068", "-157.8607"]
+    const geoArr = this.state;
     return (
       <div class='leaflet-container'>
-        <Map center={position} zoom={this.state.zoom}>
+        <Map center={position} zoom={13}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+            {geoArr.map((geoPoints) => {
+              return <Marker position={geoPoints} />
+            })}
           </Marker>
+          {/* <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup> */}
         </Map>
       </div>
 

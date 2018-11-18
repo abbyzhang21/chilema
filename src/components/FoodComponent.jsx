bcaec38481da6d3d63c84430c1140f97b0fed431
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-11-17 23:56:32 -1000

    ⚡  IMPROVE: touch-up food list page

--- src/components/FoodComponent.jsx
+++ src/components/FoodComponent.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../containers/App/App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faCircle, faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 export const FoodList = (props) => {
-    // console.log('food list:', props.foodItem)
+    console.log('food list:', props.foodItem)
 
     return props.foodItem.map(item => {
         // console.log(item.image)
@@ -15,21 +15,21 @@ export const FoodList = (props) => {
                         <img src={item.image} alt="" />
                     </div>
 
-                    <ul className='list'>
-                        <li>
-                            <a href={'food/detail/' + item.id}><h3>{item.item}</h3></a>
-                        </li>
-                        <li>
+                    <div className='food-list-info'>
+                        <a href={'food/detail/' + item.id}><h3>{item.item}</h3></a>
+                        <div className='food-list-category'>
                             {item.price}
-                            <a href="google.com">
-                                <p>{item.category}</p>
-                            </a>
-                        </li>
-                        <li>
+                            <FontAwesomeIcon icon={faGlobe} color='#666633' size='2px' className='dot-icon' />
+                            <p>
+
+                                {item.category}
+                            </p>
+                        </div>
+                        <div className='description'>
                             <FontAwesomeIcon icon={faUserCircle} color='#666633' className='user-icon' />
-                            {item.description}
-                        </li>
-                    </ul>
+                            "{item.description}"
+                        </div>
+                    </div>
 
                 </div>
             </div >

2cbf62514c606649161bcb16222033fc291ce8a7
Sarah <yamashige.sarah@gmail.com>
2018-11-21 21:03:28 -1000

    initial commit

---
+++ src/containers/EditFood/EditFood.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import GlobalHeader from '../../components/GlobalHeaderComponent';
+import axios from 'axios';
+
+
+
+class EditFood extends Component{
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return 
+        )
+    }
+}
+
+export default EditFood;

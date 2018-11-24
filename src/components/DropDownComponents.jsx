e8ddcaed4fa3a30aa73f75da360c0084918644b2
Sarah <yamashige.sarah@gmail.com>
2018-11-23 14:23:38 -1000

    dietary restriction dropdown - basic css applied

--- src/components/DropDownComponents.jsx
+++ src/components/DropDownComponents.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
 import '../stylesheets/_dropdowns.css';
-import Select from 'react-select';
+// import Select from 'react-select';
 import { BrowserRouter as Link } from 'react-router-dom'
 
-export const SearchContainer = (props) => {
+// export const SearchContainer = (props) => {
     // console.log('search options', props.foodItem)
 
-    const items = props.foodItem;
-    console.log('search container items', items)
+    // const items = props.foodItem;
+    // console.log('search container items', items)
 
-    const distinctCategory = [...new Set(items.map(item => item.category))];
-    console.log('distinctCategory items', distinctCategory)
+    // const distinctCategory = [...new Set(items.map(item => item.category))];
+    // console.log('distinctCategory items', distinctCategory)
 
     // const newItems = [...new Set(items)];
 
     // console.log('items', items)
     // console.log('distinctCategory', distinctCategory);
-    let options = [];
-    distinctCategory.map(e => {
-        let local = {};
-        local.value = e;
-        local.label = e;
-        options.push(local);
-    })
-    console.log('options', options)
-    return (
-        <div className="dropdown-container">
-            <div className="categories">
+//     let options = [];
+//     distinctCategory.map(e => {
+//         let local = {};
+//         local.value = e;
+//         local.label = e;
+//         options.push(local);
+//     })
+//     console.log('options', options)
+//     return (
+//         <div className="dropdown-container">
+//             <div className="categories">
 
-                <Select className='drop-down-elem'
-                    placeholder='what would you like to eat ...'
-                    options={options}
-                />
-            </div>
-        </div>
-    )
+//                 <Select className='drop-down-elem'
+//                     placeholder='what would you like to eat ...'
+//                     options={options}
+//                 />
+//             </div>
+//         </div>
+//     )
 
-}
+// }
 
 
 export const DietaryRestrictionComponent = () => {
